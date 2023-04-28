1d3e8d875bb2bd656578e0b5b56c2bf119c03d94
Caio <lcaio1281@gmail.com>
2023-04-28 18:19:22 -0300

    Header component

--- App.tsx
+++ App.tsx
@@ -6,9 +6,9 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 import theme from "./src/theme";
-import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
