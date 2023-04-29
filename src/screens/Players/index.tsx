d4a475efd35d1adbb5969fe87c19555486867742
Caio <lcaio1281@gmail.com>
2023-04-29 16:05:51 -0300

    finalizando lista de Players -

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,12 +7,14 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { Button } from "@components/Button";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState(["Caio", "Lucas"]);
+  const [players, setPlayers] = useState([]);
 
   return (
     <Container>
@@ -49,10 +51,20 @@ export function Players() {
       <FlatList
         data={players}
         keyExtractor={(item) => item}
+        showsVerticalScrollIndicator={false}
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
       />
+
+      <Button title="Remover turmas" type="SECONDARY" />
     </Container>
   );
 }
