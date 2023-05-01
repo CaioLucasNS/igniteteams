93862907b890d199ec4ce6fb64635d8deacf54e7
Caio <lcaio1281@gmail.com>
2023-05-01 00:23:22 -0300

    listando players

--- src/storage/player/playerAddByGroup.ts
+++ src/storage/player/playerAddByGroup.ts
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AppError } from "@utils/AppError";
 
 import { PLAYER_COLLECTION } from "@storage/storageConfig";
-
+import { playersGetByGroup } from "./playersGetByGroup";
 import { PlayerStorageDTO } from "./PlayerStorageDTO";
 
 export async function playerAddByGroup(
@@ -10,11 +10,23 @@ export async function playerAddByGroup(
   group: string
 ) {
   try {
+    const storedPlayers = await playersGetByGroup(group);
+
+    const playerAlreadyExists = storedPlayers.filter(
+      (player) => player.name === newPLayer.name
+    );
+
+    if (playerAlreadyExists.length > 0) {
+      throw new AppError("Essa pessoa já faz pate de um time aqui.");
+    }
+
+    const storage = JSON.stringify([...storedPlayers, newPLayer]);
+
     /**
      * @ignite-teams:players-group
      * >> @ignite-teams:players-exampleGroup: [players]
      */
-    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, "");
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage);
   } catch (error) {
     throw error;
   }
